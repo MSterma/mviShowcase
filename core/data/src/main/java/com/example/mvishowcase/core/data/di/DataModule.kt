22ba712ca44bf54ef5f29d336114725f793fd12a
Mateusz Sterma <mateusz.sterma0703@gmail.com>
2026-07-03 11:26:55 +0200

    cosmetic changes. Found factoryOf somewhere on internet and i remember correctly this structure was used in project shown during meeting so i change my di to use it. I also moved definig bearer to network module, because i thought it'd be cleaner to provide all needed for repository data in one file even if it's constant

--- core/data/src/main/java/com/example/mvishowcase/core/data/di/DataModule.kt
+++ core/data/src/main/java/com/example/mvishowcase/core/data/di/DataModule.kt
@@ -2,13 +2,11 @@ package com.example.mvishowcase.core.data.di
 
 import com.example.mvishowcase.core.data.repository.CountryRepositoryImpl
 import com.example.mvishowcase.core.domain.repository.CountryRepository
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<CountryRepository> { 
-        CountryRepositoryImpl(
-            httpClient = get(),
-            bearerToken = ""
-        )
-    }
+    single<CountryRepository> { CountryRepositoryImpl(get(), get(named("bearer_token"))) }
 }
--- core/network/src/main/java/com/example/mvishowcase/core/network/NetworkModule.kt
+++ core/network/src/main/java/com/example/mvishowcase/core/network/NetworkModule.kt
@@ -1,7 +1,9 @@
 package com.example.mvishowcase.core.network
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val networkModule = module {
     single { NetworkClient.httpClient }
+    single(named("bearer_token")) { "" }
 }
